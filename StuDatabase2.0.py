c557b6df1ccd25a5e8639eb0fc0875a876983d7a
Shenedia Ellington <artistic.nita@gmail.com>
2024-01-27 15:20:41 -0500

    main assignment completed

--- StuDatabase2.0.py
+++ StuDatabase2.0.py
@@ -1,77 +1,35 @@
-
+#  dictionary list
 student_names = [{"name": "Tina", "hometown": "Portland", "favorite_food": "puppy chow"},
-              {"name": "Klaus", "hometown": "Frankfurt", "favorite_food": "pizza"},
-              {"name": "Julia", "hometown": "Houston", "favorite_food": "shrimp"}, ]
+                 {"name": "Klaus", "hometown": "Frankfurt", "favorite_food": "pizza"},
+                 {"name": "Julia", "hometown": "Houston", "favorite_food": "shrimp"}, ]
 
-def get_selection():
-    get_student = {}
-    get_student["name"] = input("Please enter the student name: ")
-    get_student["hometown"] = input("Please enter the student hometown: ")
-    get_student["favorite food"] = input("Please enter the student favorite food: ")
-    return get_student
 
-def list_names(names):
+# def function to add onto student_names list in while true/if loop
+def get_new_student():
+    new_student = {}
+    new_student["name"] = input("Please enter the student first name:> ")
+    new_student["hometown"] = input("Please enter the student hometown:> ")
+    new_student["favorite food"] = input("Please enter the student favorite food:> ")
+    return new_student
+
+
+# def function to print student names in while true/elif loop
+def print_student_names(names):
     for student in names:
-        names.index(student) +1, student['name']
-
-        list_names(student_names)
+        print(names.index(student) + 1, student['name'])
 
 
-while True:
-    user_input = input("Please select which action you'd like to do: add, view, or quit? ")
-    if user_input == 'add':
-          get_student = get_selection()
-          list_names.append(student_names)
-          print(list_names)
-    # elif user_input == 'view':
-
-
-
-
-
-    #     if 0 <= greeting - 1 < len(names):
-    #      print(f"Student {names[greeting - 1]}. What would you like to know about this student?")
-    #      response = input('Enter "hometown" or "favorite food": ')
-    #      if response == "hometown":
-    #          print(f"{names[greeting - 1]}'s hometown is {home_town[greeting - 1]}.")
-    #     elif response == "favorite food":
-    #         print(f"{names[greeting - 1]}'s favorite food is {favorite_food[greeting - 1]}.")
-    #     else:
-    #          print("Invalid response. Please try again.")
-    #
-    # elif user_input == 'quit':
-    #     print("Good bye")
-    #     break
-    # else:
-    #     print("Invalid statement. Please tray again.")
-
-
-
-
-
-
-
-
-
-# go_on = 'y'
-
-# while True:
-# student_option = "notstarted"
-
-# while(not student_option.isdigit()\
-#         or not(int(student_option) >0\
-#         and int(student_option) <= len(list_names))):
-#     print(f"Please select which action you'd like to do: add, view, or quit? ")
-#     student_option = input("> ")
-
-
-
-#
-# while True:
-#     try:
-#         greeting = int(input("Welcome! Which student would you like to learn more about? Enter a number 1-4 (Enter 0 to exit): "))
-#         if greeting == 0:
-#             break
-#         students(names, home_town, favorite_food)
-#     except ValueError:
-#         print("Invalid entry. Please enter a valid number.")
+while True:  # will repeat everything in this loop depending on operational choices
+    print("Welcome to the Student Database Version2.0\n")
+    user_input = input('Please select which action you would like to take: "add", "view", or "quit"?\n>')
+    if user_input == 'add':  # if section for def function get_new_student() to add on new student information
+        new_student = get_new_student()
+        student_names.append(new_student)
+        print(f"Update Successful!\n{new_student}")
+    elif user_input == 'view':  # elif section for def funct print_student_names to show list of student names from dict list student_names.
+        print_student_names(student_names)
+    elif user_input == 'quit':
+        print("GOOD BYE!")
+        break
+    else:
+        print("Invalid statement. Please tray again.\n")
