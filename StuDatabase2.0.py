0fa36b322f614f182e2213c3a80bb0e655020e81
Shenedia Ellington <artistic.nita@gmail.com>
2024-01-28 20:14:54 -0500

    added Pseudo code via comments at the top of code

--- StuDatabase2.0.py
+++ StuDatabase2.0.py
@@ -1,3 +1,16 @@
+''' Pseudo_code
+create one define function named: list_names
+read dict list student_names and loops through list and prints out student name
+use index +1 to have names read with 1 for user.
+
+create one define function named: get_new_student
+user input will be stored in function and add to existing dict list student_names
+use .append() to add new entry from user input() to the end of dict list student_names
+
+add while loop to run the input() prompts
+add if/elif/else to while loop to prompt user to enter new student information to dict list or view existing
+student information in dict list or to end program with a goodbye! message.'''
+
 #  dictionary list
 student_names = [{"name": "Tina", "hometown": "Portland", "favorite_food": "puppy chow"},
                  {"name": "Klaus", "hometown": "Frankfurt", "favorite_food": "pizza"},
@@ -29,7 +42,7 @@ while True:  # will repeat everything in this loop depending on operational choi
     elif user_input == 'view':  # elif section for def funct print_student_names to show list of student names from dict list student_names.
         print_student_names(student_names)
     elif user_input == 'quit':
-        print("GOOD BYE!")
+        print("GOODBYE!")
         break
     else:
         print("Invalid statement. Please tray again.\n")
